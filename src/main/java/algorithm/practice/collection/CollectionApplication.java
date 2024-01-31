29e2c61f380befdbfb9e690474b75974e0b3a2a4
Yahia Nurhussien <yahianurhussien@Yahias-MacBook-Pro.local>
2024-01-30 22:00:18 -0600

    adding main class

---
+++ src/main/java/algorithm/practice/collection/CollectionApplication.java
@@ -0,0 +1,15 @@
+package algorithm.practice.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Entered to Main() method........... ");
+        SpringApplication.run(CollectionApplication.class, args);
+    }
+
+}
