daa3ea4dc8997f137a9a449dfeb9b5152619efe8
Yahia Nurhussien <yahianurhussien@Yahias-MacBook-Pro.local>
2024-01-30 22:16:50 -0600

    string tokens extract from a given string - added

---
+++ src/main/java/algorithm/practice/collection/StringToken.java
@@ -0,0 +1,35 @@
+package algorithm.practice.collection;
+
+import java.util.Scanner;
+
+public class StringToken {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+
+        String[] tokens = s.trim().split("[ !,?._'@]+");
+
+        if( s.trim().isEmpty()){
+            System.out.println("0");
+        }else {
+            System.out.println(tokens.length);
+            for (String str: tokens){
+                System.out.println(str);
+            }
+        }
+
+
+
+        scan.close();
+
+        // input example'
+        // He is a very very good boy, isn't he?
+        //
+
+    }
+
+
+
+}
