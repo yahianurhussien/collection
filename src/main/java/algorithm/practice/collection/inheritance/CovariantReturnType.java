9dfbfa6d05b193bae157bee888e05485a9f00d96
Yahia Nurhussien <yahianurhussien@Yahias-MacBook-Pro.local>
2024-02-10 17:28:00 -0600

    covariant return type, inheritance implementation

---
+++ src/main/java/algorithm/practice/collection/inheritance/CovariantReturnType.java
@@ -0,0 +1,76 @@
+package algorithm.practice.collection.inheritance;
+
+import java.awt.datatransfer.FlavorEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+//Complete the classes below
+class Flower {
+
+    public String whatsYourName() {
+        return "I have many names and types";
+    }
+
+}
+
+class Jasmine extends Flower {
+    @Override
+    public String whatsYourName() {
+        return "Jasmine";
+    }
+
+}
+
+class Lily extends Flower{
+    @Override
+    public String whatsYourName() {
+        return "Lily";
+    }
+
+}
+
+class Region {
+
+    public Flower yourNationalFlower() {
+        return new Flower();
+
+    }
+}
+class WestBengal extends Region {
+    @Override
+    public Flower yourNationalFlower(){
+        return new Jasmine();
+
+    }
+}
+
+class AndhraPradesh extends Region{
+    @Override
+    public Flower yourNationalFlower(){
+        return new Lily();
+
+    }
+}
+
+public class CovariantReturnType {
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String s = reader.readLine().trim();
+            Region region = null;
+            switch (s) {
+                case "WestBengal":
+                    region = new WestBengal();
+                    break;
+                case "AndhraPradesh":
+                    region = new AndhraPradesh();
+                    break;
+            }
+            Flower flower = region.yourNationalFlower();
+            System.out.println(flower.whatsYourName());
+        }
+    }
+
