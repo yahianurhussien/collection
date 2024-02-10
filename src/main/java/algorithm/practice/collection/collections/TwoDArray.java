f1508c0a21dc79e884890c977faefafb9fde84e9
Yahia Nurhussien <yahianurhussien@Yahias-MacBook-Pro.local>
2024-02-10 00:24:16 -0600

    two dimentional array largest hourglass added

--- src/main/java/algorithm/practice/collection/collections/TwoDArray.java
+++ src/main/java/algorithm/practice/collection/collections/TwoDArray.java
@@ -34,6 +34,58 @@ public class TwoDArray {
         bufferedReader.close();
 
         System.out.println(arr);
+        System.out.println(largestSumHourGlass(arr));
+
     }
+
+    private static int largestSumHourGlass(List<List<Integer>> arr) {
+
+        int largest = Integer.MIN_VALUE;
+
+        for ( int row = 0; row < arr.size() - 2; row++){
+
+
+            for( int column = 0; column < arr.size()-2; column++ ){
+                int   sum =      arr.get(row).get(column)
+                        + arr.get(row).get(column+1)
+                        + arr.get(row).get(column+ 2)
+                        + arr.get(row+1).get(column+1)
+                        + arr.get(row+2).get(column)
+                        + arr.get(row+2).get(column+ 1)
+                        + arr.get(row+2).get(column+ 2);
+
+                if (sum > largest)
+                    largest = sum;
+
+
+            }
+
+        }
+
+
+
+        return largest;
+
+
+    }
+
+
+//    sample input
+//    1 1 1 0 0 0
+//    0 1 0 0 0 0
+//    1 1 1 0 0 0
+//    0 0 2 4 4 0
+//    0 0 0 2 0 0
+//    0 0 1 2 4 0
+
+//    checks
+//    a b c
+//      d
+//    e f g
+
+//    output
+//    19
+
+
 }
 
