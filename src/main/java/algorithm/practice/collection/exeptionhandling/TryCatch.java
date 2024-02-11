32a974a82058617812e72e7c599a86cbb195d3bc
Yahia Nurhussien <yahianurhussien@Yahias-MacBook-Pro.local>
2024-02-10 21:12:25 -0600

    try catch exception handling example added

---
+++ src/main/java/algorithm/practice/collection/exeptionhandling/TryCatch.java
@@ -0,0 +1,30 @@
+package algorithm.practice.collection.exeptionhandling;
+
+import java.io.*;
+import java.util.*;
+
+public class TryCatch {
+
+
+        public static void main(String[] args) {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+
+            Scanner scanner = new Scanner(System.in);
+
+            try{
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+
+                System.out.println(x/y);
+
+            }catch(InputMismatchException e){
+                System.out.println(e);
+            }catch(ArithmeticException e ){
+                System.out.println(e);
+            }
+
+        }
+    }
+
+
