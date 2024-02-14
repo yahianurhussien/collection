90515edea40060aa328b7d5dd3264375e04bacdd
Yahia Nurhussien <yahianurhussien@Yahias-MacBook-Pro.local>
2024-02-14 17:11:18 -0600

    majority element check algorithm added

---
+++ src/main/java/algorithm/practice/collection/collections/MajorityElement.java
@@ -0,0 +1,41 @@
+package algorithm.practice.collection.collections;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class MajorityElement {
+
+    public static void main(String[] args) {
+        System.out.println(majorityElement(new int[]{2,2,1,1,1,2,2}));
+        System.out.println(majorityElementEfficient(new int[]{2,2,3,3,3,3,2}));
+    }
+
+    private static int majorityElement(int[] nums) {
+        int majoritySize = nums.length / 2;
+
+        Arrays.sort(nums);
+        int start = nums[1];
+        int count  = 1;
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i] == nums[i-1]){
+                count++;
+            }else{
+                count = 1;
+            }
+            if (count > majoritySize){
+                return nums[i];
+            }
+        }
+     return 0;
+    }
+
+    private static int majorityElementEfficient(int[] nums){
+       Arrays.sort(nums);
+
+       return nums[nums.length / 2];
+
+
+
+    }
+}
